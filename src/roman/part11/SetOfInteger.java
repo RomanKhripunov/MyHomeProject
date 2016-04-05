d56ab4e5c3482f4653b2e36ad81bf4c1582ffd3d
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-06 00:07:49 +0300

    page 294

---
+++ src/roman/part11/SetOfInteger.java
@@ -0,0 +1,19 @@
+package roman.part11;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class SetOfInteger {
+
+    public static void main(String[] args) {
+        Random random = new Random(47);
+        Set<Integer> intSet = new HashSet<Integer>();
+
+        for (int i = 0; i < 10000; i++) {
+            intSet.add(random.nextInt(30));
+        }
+
+        System.out.println(intSet);
+    }
+}
