616b6388e09f577a254d642e909d2af999ba0099
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-11 23:54:25 +0300

    page 309 (Резюме главы)

---
+++ src/roman/part11/EnvironmentVariables.java
@@ -0,0 +1,12 @@
+package roman.part11;
+
+import java.util.Map;
+
+public class EnvironmentVariables {
+
+    public static void main(String[] args) {
+        for (Map.Entry entry : System.getenv().entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
