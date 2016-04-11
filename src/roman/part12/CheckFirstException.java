85faf4a01c42df4505a7e641069c88d2a443b6b2
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-12 00:07:35 +0300

    page 310 (Exceptions)

---
+++ src/roman/part12/CheckFirstException.java
@@ -0,0 +1,11 @@
+package roman.part12;
+
+public class CheckFirstException {
+
+    public static void main(String[] args) {
+        String t = null;
+        if (t == null) {
+            throw new NullPointerException();
+        }
+    }
+}
