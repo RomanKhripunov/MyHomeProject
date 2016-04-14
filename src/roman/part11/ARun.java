906e1dad617ac62a489595267456aa75bdcca175
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-15 00:16:07 +0300

    Sergey's task

--- src/roman/part11/ARun.java
+++ src/roman/part11/ARun.java
@@ -3,12 +3,21 @@ package roman.part11;
 public class ARun {
 
     public static void main(String[] args) {
-
+        B2Action b2Action = new B2Action();
+        B3 b3 = new B3();
+        b2Action.prepare(b3);
     }
 }
-/*
 
-interface V1 {
+class B3 implements V2 {
+    @Override
+    public int getValue() {
+        return 0;
+    }
+}
+
+
+interface V1<T> {
     int getValue();
 }
 
@@ -17,13 +26,13 @@ interface V2 extends V1 {
 }
 
 interface IAction {
-    int prepare(V1 v1);
+    <T1> int prepare(V1<? extends T1> v1);
 }
 
 class MainAction implements IAction {
 
     @Override
-    public int prepare(V1 v1) {
+    public <T1> int prepare(V1<? extends T1> v1) {
         return v1.getValue() * 2;
     }
 }
@@ -31,7 +40,7 @@ class MainAction implements IAction {
 class B1Action extends MainAction {
 
     @Override
-    public int prepare(V1 v1) {
+    public <T1> int prepare(V1<? extends T1> v1) {
         return v1.getValue() * 3;
     }
 }
@@ -39,7 +48,7 @@ class B1Action extends MainAction {
 class B2Action extends MainAction {
 
     @Override
-    public int prepare(V2 v2) {
+    public <T1> int prepare(V1<? extends T1> v2) {
         return v2.getValue() * 4;
     }
-}*/
+}
