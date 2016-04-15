04a17c137c8ca2bafb4ca7c1c81699769b24474f
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-15 21:52:10 +0300

    part 12 page 315 (exceptions)

---
+++ src/roman/part12/InheritingException.java
@@ -0,0 +1,22 @@
+package roman.part12;
+
+class SimpleException extends Exception {
+}
+
+public class InheritingException {
+
+    public void f() throws SimpleException {
+        System.out.println("Возбуждаем SimpleException из f()");
+        throw new SimpleException();
+    }
+
+    public static void main(String[] args) {
+        InheritingException sed = new InheritingException();
+
+        try {
+            sed.f();
+        } catch (SimpleException e) {
+            System.out.println("Перехвачено!");
+        }
+    }
+}
