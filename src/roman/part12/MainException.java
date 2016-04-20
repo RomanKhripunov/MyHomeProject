3b730caaa8438ca5fe94dfce190543d16c9ac6b4
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-21 00:04:42 +0300

    part 12 page 348 (mainException)

---
+++ src/roman/part12/MainException.java
@@ -0,0 +1,11 @@
+package roman.part12;
+
+import java.io.FileInputStream;
+
+public class MainException {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream file = new FileInputStream("D:\\Java\\MyHomeProject\\src\\roman\\part12\\MainException.java");
+        file.close();
+    }
+}
