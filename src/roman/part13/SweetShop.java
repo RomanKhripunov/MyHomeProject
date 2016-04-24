745af499da7b8858e960d301ee02804416d88107
RomanKhripunov <roma.khripunov@gmail.com>
2016-04-24 13:34:17 +0300

    part 13 page 355 (объект Class)

---
+++ src/roman/part13/SweetShop.java
@@ -0,0 +1,43 @@
+package roman.part13;
+
+class Candy {
+
+    static {
+        System.out.println("Загрузка класса Candy");
+    }
+}
+
+class Gum {
+
+    static {
+        System.out.println("Загрузка класса Gum");
+    }
+}
+
+class Cookie {
+
+    static {
+        System.out.println("Загрузка класса Cookie");
+    }
+}
+
+public class SweetShop {
+
+    public static void main(String[] args) {
+        System.out.println("В методе main()");
+
+        new Candy();
+        System.out.println("После создания объекта Candy");
+
+        try {
+            Class.forName("Gum");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Класс Gum не найден");
+        }
+
+        System.out.println("После вызова метода Class.forName(\"Gum\")");
+
+        new Cookie();
+        System.out.println("После создания объекта Cookie");
+    }
+}
