6b98323d782f6149b6c193ccebe2ad64a57f2e64
RomanKhripunov <roma.khripunov@gmail.com>
2016-05-02 00:09:45 +0300

    part 14 page 409 (вычисление типа аргумента)

---
+++ src/roman/part14/SimplerPets.java
@@ -0,0 +1,14 @@
+package roman.part14;
+
+import roman.part13.Person;
+import roman.part13.Pet;
+
+import java.util.List;
+import java.util.Map;
+
+public class SimplerPets {
+
+    public static void main(String[] args) {
+        Map<Person, List<? extends Pet>> petPeople = New.map();
+    }
+}
