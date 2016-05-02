39985d74dfdf82a8f925710f25dcddffdf2a644e
RomanKhripunov <roma.khripunov@gmail.com>
2016-05-02 23:26:13 +0300

    part 14 page 411 (параметризованные методы и генераторы)

---
+++ src/roman/part14/Generators.java
@@ -0,0 +1,24 @@
+package roman.part14;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Generators {
+
+    public static <T> Collection<T> fill(Collection<T> coll, Generator<T> gen, int n) {
+        for (int i = 0; i < n; i++) {
+            coll.add(gen.next());
+        }
+        return coll;
+    }
+
+    public static void main(String[] args) {
+        Collection<Coffee> coffee = fill(new ArrayList<Coffee>(), new CoffeeGenerator(), 4);
+        coffee.forEach(System.out::println);
+
+        Collection<Integer> fnumbers = fill(new ArrayList<Integer>(), new Fibonacci(), 12);
+        for (int i : fnumbers) {
+            System.out.print(i + ", ");
+        }
+    }
+}
