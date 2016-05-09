917bae2a10628e96456b5e1751b7e12270440676
RomanKhripunov <roma.khripunov@gmail.com>
2016-05-10 00:30:59 +0300

    part 14 page 429 (проблемы на границах)

---
+++ src/roman/part14/GenericHolder.java
@@ -0,0 +1,20 @@
+package roman.part14;
+
+public class GenericHolder<T> {
+
+    private T obj;
+
+    public void set(T obj) {
+        this.obj = obj;
+    }
+
+    public T get() {
+        return obj;
+    }
+
+    public static void main(String[] args) {
+        GenericHolder<String> holder = new GenericHolder<>();
+        holder.set("item");
+        String s = holder.get();
+    }
+}
