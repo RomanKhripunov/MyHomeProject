57038acd16266c6c801c39bf72fe4c0517e690da
r.hripunov <roma.khripunov@gmail.com>
2016-05-27 16:24:33 +0300

    create form with button

---
+++ src/TimeTracker/SimpleFrame.java
@@ -0,0 +1,28 @@
+package TimeTracker;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleFrame {
+
+    public void start() {
+        JFrame frame = new JFrame("TimeTracker");
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        JButton startButton = new JButton("Start");
+        startButton.setBackground(Color.green);
+        startButton.setBounds(100, 100, 100, 100);
+
+        panel.add(startButton);
+    }
+
+    public static void main(String[] args) {
+        new SimpleFrame().start();
+    }
+}
