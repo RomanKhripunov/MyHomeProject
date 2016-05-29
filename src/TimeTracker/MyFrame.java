5b73049b092c7e3aeb8085291bfe4c4061598c34
RomanKhripunov <roma.khripunov@gmail.com>
2016-05-29 23:17:30 +0300

    implementation start method

--- src/TimeTracker/MyFrame.java
+++ src/TimeTracker/MyFrame.java
@@ -4,36 +4,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyFrame extends JFrame {
 
     private static String windowName = "TimeTracker";
-    JButton buttonStart, buttonPause, buttonStop;
+    JButton buttonStart, buttonPause, buttonStop, buttonReset;
     JLabel labelTaskDescription;
-    JTextField taskDescription;
+    JTextField filedTaskDescription;
     EventHandler eventHandler = new EventHandler();
+    String startTime, taskDescription, endTime;
 
     public MyFrame() {
         this(windowName);
+
     }
 
     public MyFrame(String newWindowName) {
         super(newWindowName);
         setLayout(new FlowLayout());
+        buttonReset = new JButton("Reset");
         buttonStart = new JButton("Start");
-        buttonPause = new JButton("Pause");
+//        buttonPause = new JButton("Pause");
         buttonStop = new JButton("Stop");
         labelTaskDescription = new JLabel("Task description");
-        taskDescription = new JTextField(15);
+        filedTaskDescription = new JTextField(15);
 
+        add(buttonReset);
         add(buttonStart);
-        add(buttonPause);
+//        add(buttonPause);
         add(buttonStop);
         add(labelTaskDescription);
-        add(taskDescription);
+        add(filedTaskDescription);
 
+        buttonReset.addActionListener(eventHandler);
         buttonStart.addActionListener(eventHandler);
-        buttonPause.addActionListener(eventHandler);
+//        buttonPause.addActionListener(eventHandler);
         buttonStop.addActionListener(eventHandler);
     }
 
@@ -56,21 +63,41 @@ public class MyFrame extends JFrame {
                 pauseTrack();
             } else if (event.getSource() == buttonStop) {
                 stopTrack();
+            } else if (event.getSource() == buttonReset) {
+                resetTrack();
             }
         }
     }
 
-    public String startTrack() {
-        String taskDescription;
-        taskDescription = JOptionPane.showInputDialog(null, "Please, type the description of task", windowName, JOptionPane.INFORMATION_MESSAGE);
-        return taskDescription;
+    private void resetTrack() {
+        startTime = null;
+        taskDescription = null;
+        endTime = null;
+    }
+
+    public void startTrack() {
+        while (true) {
+            try {
+                taskDescription = JOptionPane.showInputDialog(null,
+                        "Please, type the description of task",
+                        windowName,
+                        JOptionPane.INFORMATION_MESSAGE);
+                if (taskDescription == null || "".equals(taskDescription.replaceAll("\\s+", ""))) {
+                    throw new IllegalAccessException();
+                }
+                break;
+            } catch (IllegalAccessException e) {
+                JOptionPane.showMessageDialog(null, "Task description cannot be empty!", windowName, JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        startTime = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
     }
 
     public void pauseTrack() {
-        JOptionPane.showMessageDialog(null, "Pause", windowName, JOptionPane.INFORMATION_MESSAGE);
+        // todo
     }
 
     public void stopTrack() {
-        JOptionPane.showMessageDialog(null, "Stop", windowName, JOptionPane.ERROR_MESSAGE);
+        endTime = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
     }
 }
---
+++ src/TimeTracker/TestSplit.java
@@ -0,0 +1,10 @@
+package TimeTracker;
+
+public class TestSplit {
+
+    public static void main(String[] args) {
+        String test = "      ";
+        System.out.println("[" + test.replaceAll("\\s+", "") + "]");
+
+    }
+}
