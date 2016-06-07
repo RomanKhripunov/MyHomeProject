a1e4782f0af2c34b231f3137dc21be0a77b62e8a
r.hripunov <roma.khripunov@gmail.com>
2016-06-07 10:06:50 +0300

    format code

--- src/Calculator/MyRPNBuilder.java
+++ src/Calculator/MyRPNBuilder.java
@@ -6,17 +6,17 @@ import java.io.InputStreamReader;
 
 public class MyRPNBuilder {
 
-    public String readExpression() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please, type the your expression:");
-        String textExpression = null;
-        try {
-            textExpression = reader.readLine().trim().replaceAll("\\s+", "");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        return textExpression;
-    }
+//    public String readExpression() {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Please, type the your expression:");
+//        String textExpression = null;
+//        try {
+//            textExpression = reader.readLine().trim().replaceAll("\\s+", "");
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return textExpression;
+//    }
 
     public String[] convertToRPN(String inputString) {
         return inputString.split("[\\\\s0-9+-]+");
